f4f39cee0226fe2e0e83f2523bd4235c549383d0
Ваше ИмяSergey <Sergey43773@gmail.com>
2021-01-28 17:11:42 +0700

    empty

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from recipes import serializers
-from recipes.models import Favorites
-
-
-class FavoritesSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Favorites
-        fields = ("user", "recipe")
