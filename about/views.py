1289acd57ed7ce2f5fee4f5c3ce369fcc97f179d
Ваше ИмяSergey <Sergey43773@gmail.com>
2021-02-02 19:38:11 +0700

    add workflow

--- about/views.py
+++ about/views.py
@@ -6,6 +6,16 @@ class AboutAuthorView(TemplateView):
 
 
 class AboutTechView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        type_list = [['Для создания сайта:', 'Python v:3.7.6',
+                      'PostgreSQL v:12.5', 'Javascript', 'HTML 5', 'CSS 3'],
+                     ['Для разворачивания на боевом сервере:', 'Docker',
+                      'Nginx', 'Gunicorn'],
+                     ['Для автаматического деплоя:', 'GitHub workflow']]
+        context['type_list'] = type_list
+        return context
+
     template_name = "tech.html"
 
 
