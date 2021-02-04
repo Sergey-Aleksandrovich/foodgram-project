ded0c527df2555c7d573672e1b4b60b38945ad6a
Ваше ИмяSergey <Sergey43773@gmail.com>
2021-02-04 16:24:50 +0700

    fir forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -6,17 +5,9 @@ User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput,
-                                help_text='Ваш пароль должен содержать как '
-                                          'минимум 8 символов.')
-
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("username", "email")
         labels = {'username': 'Логин',
                   'email': 'Адрес электронной почты',
                   }
-
-
-
-
