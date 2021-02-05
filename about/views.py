3b47273dead0663ad4f392c625a8463f3c71a42f
Ваше ИмяSergey <Sergey43773@gmail.com>
2021-02-05 18:26:01 +0700

    fix tech

--- about/views.py
+++ about/views.py
@@ -8,8 +8,11 @@ class AboutAuthorView(TemplateView):
 class AboutTechView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        type_list = [['Для создания сайта:', 'Python v:3.7.6',
-                      'PostgreSQL v:12.5', 'Javascript', 'HTML 5', 'CSS 3'],
+
+        type_list = [['Для создания сайта:', 'Django v:3.1.2', 'DRF v:3.12.2 ',
+                      'Python v:3.7.6', 'PostgreSQL v:12.5', 'Javascript',
+                      'HTML 5',
+                      'CSS 3'],
                      ['Для разворачивания на боевом сервере:', 'Docker',
                       'Nginx', 'Gunicorn'],
                      ['Для автаматического деплоя:', 'GitHub workflow']]
