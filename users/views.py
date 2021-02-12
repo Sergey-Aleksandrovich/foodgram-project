94b7b99ac7792961b817c980ac25fba99a2d0045
Ваше ИмяSergey <Sergey43773@gmail.com>
2021-02-12 18:01:25 +0700

    auto login after reg

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login, authenticate
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
@@ -10,9 +11,15 @@ class SignUp(CreateView):
     success_url = reverse_lazy("signup_success")
     template_name = "users/reg.html"
 
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password1"],
+        )
+        login(self.request, user)
+        return to_return
+
 
 class SignupSuccess(TemplateView):
     template_name = "users/reg_done.html"
-
-
-
